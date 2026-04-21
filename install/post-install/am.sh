ac3ce57d6ab27fc12cc3b65194764d7dc8b1ae00
Sykes <74189099+SykesTheLord@users.noreply.github.com>
2026-04-21 23:49:08 +0200

    fix issues

--- install/post-install/am.sh
+++ install/post-install/am.sh
@@ -1,6 +1,9 @@
 # Install AM package manager
 [ -z "$AM_BRANCH" ] && AM_BRANCH="main"
 
+# The ISO builder stages AM here so the installer works offline.
+AM_OFFLINE_DIR="/opt/packages/am"
+
 sudo mkdir -p /opt/am/modules /usr/local/bin
 
 sudo tee /opt/am/remove >/dev/null <<'EOF'
@@ -14,7 +17,9 @@ rm -R -f /opt/am
 EOF
 sudo chmod a+x /opt/am/remove
 
-if ! sudo wget -q -O /opt/am/APP-MANAGER "https://raw.githubusercontent.com/ivan-hc/AM/$AM_BRANCH/APP-MANAGER"; then
+if [[ -f $AM_OFFLINE_DIR/APP-MANAGER ]]; then
+  sudo cp "$AM_OFFLINE_DIR/APP-MANAGER" /opt/am/APP-MANAGER
+elif ! sudo wget -q -O /opt/am/APP-MANAGER "https://raw.githubusercontent.com/ivan-hc/AM/$AM_BRANCH/APP-MANAGER"; then
   echo "ERROR: Cannot download AM"
   exit 1
 fi
@@ -29,17 +34,23 @@ else
   sudo ln -fs /opt/am/APP-MANAGER /usr/local/bin/am
 fi
 
-MODULES=$(curl -Ls "https://raw.githubusercontent.com/ivan-hc/AM/$AM_BRANCH/APP-MANAGER" | tr '"' '\n' | grep "[a-z]\.am$")
+MODULES=$(tr '"' '\n' </opt/am/APP-MANAGER | grep "[a-z]\.am$")
 if [[ -z $MODULES ]]; then
-  echo "ERROR: Cannot download AM modules"
+  echo "ERROR: Cannot determine AM modules"
   exit 1
 fi
 
 for module_name in $MODULES; do
-  if [[ ! -f /opt/am/modules/$module_name ]]; then
-    sudo wget -q -O "/opt/am/modules/$module_name" "https://raw.githubusercontent.com/ivan-hc/AM/$AM_BRANCH/modules/$module_name"
-    sudo chmod a+x "/opt/am/modules/$module_name"
+  if [[ -f /opt/am/modules/$module_name ]]; then
+    continue
   fi
+  if [[ -f $AM_OFFLINE_DIR/modules/$module_name ]]; then
+    sudo cp "$AM_OFFLINE_DIR/modules/$module_name" "/opt/am/modules/$module_name"
+  elif ! sudo wget -q -O "/opt/am/modules/$module_name" "https://raw.githubusercontent.com/ivan-hc/AM/$AM_BRANCH/modules/$module_name"; then
+    echo "ERROR: Cannot fetch AM module $module_name"
+    exit 1
+  fi
+  sudo chmod a+x "/opt/am/modules/$module_name"
 done
 
 sudo chown -R "$USER" /opt/am
--- install/post-install/zsh-install.sh
+++ install/post-install/zsh-install.sh
@@ -3,6 +3,9 @@ wget -q -O /tmp/omz-install.sh https://raw.githubusercontent.com/ohmyzsh/ohmyzsh
 bash /tmp/omz-install.sh --unattended --keep-zshrc --skip-chsh
 rm -f /tmp/omz-install.sh
 
+# Make zsh the login shell (omz was run with --skip-chsh)
+sudo chsh -s /usr/bin/zsh "$USER"
+
 # Install Oh My Zsh plugins
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
 git clone https://github.com/zsh-users/zsh-autosuggestions "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
