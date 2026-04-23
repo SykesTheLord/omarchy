34c3ee69cb3a6a3abfb96a463b6cba83fd327406
Sykes <74189099+SykesTheLord@users.noreply.github.com>
2026-04-23 11:52:27 +0200

    Implement Custom setup

--- install/post-install/all.sh
+++ install/post-install/all.sh
@@ -1,8 +1,7 @@
 run_logged $OMARCHY_INSTALL/post-install/hibernation.sh
 run_logged $OMARCHY_INSTALL/post-install/pacman.sh
-# run_logged $OMARCHY_INSTALL/post-install/am.sh
 run_logged $OMARCHY_INSTALL/post-install/zsh-install.sh
-# run_logged $OMARCHY_INSTALL/post-install/am-install-apps.sh
+run_logged $OMARCHY_INSTALL/post-install/user-dotfiles.sh
 run_logged $OMARCHY_INSTALL/post-install/secure-boot.sh
 source $OMARCHY_INSTALL/post-install/allow-reboot.sh
 source $OMARCHY_INSTALL/post-install/finished.sh
